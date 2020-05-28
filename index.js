981973c021b3c0b070ba571c95d0e8176657bc89
varapsd <varaprasad3638@gmail.com>
2020-05-28 15:16:03 +0530

    changed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 
 const PORT = process.env.PORT || 5000;
 app.get('/',(req,res)=>{
-	res.send("let me know the port" + PORT);
+	res.send("choo cute babyy :)");
 })
 
 app.listen(PORT,(err,data)=> console.log('listening at ',PORT));
