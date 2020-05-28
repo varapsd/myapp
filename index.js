92520a40d36dbacb86687bbf7eaacb509cb8795a
varapsd <varaprasad3638@gmail.com>
2020-05-28 15:10:33 +0530

    porttest

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 
 const PORT = process.env.PORT || 5000;
 app.get('/',(req,res)=>{
-	res.send("hello World");
+	res.send("let me know the port" + PORT);
 })
 
 app.listen(PORT,(err,data)=> console.log('listening at ',PORT));
