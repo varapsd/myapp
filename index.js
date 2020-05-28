f6daf2e7729ad2477d6541e67888ee7a19a70785
varapsd <varaprasad3638@gmail.com>
2020-05-28 22:45:26 +0530

    login

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
+app.use(express.static(__dirname + '/public'));
 
+app.set('views engine','ejs');
 const PORT = process.env.PORT || 5000;
 app.get('/',(req,res)=>{
-	res.send("choo cute babyy :)");
+	res.render('login.ejs');
 })
 
 app.listen(PORT,(err,data)=> console.log('listening at ',PORT));
